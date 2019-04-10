fa3174566d1849c59472f75221d9605083bf4d6e
Paulo Penteado <ppenteado@gdginc.local>
2019-04-10 11:06:12 -0400

    1. application.properties updated for DB access; 2. pacient entity created; 3. pacient DAO interface and implementation created; 4. REST Controller created with @GetMapping

---
+++ src/main/java/com/didactic/clinic/cruddemo/dao/PacientDAO.java
@@ -0,0 +1,10 @@
+package com.didactic.clinic.cruddemo.dao;
+
+import java.util.List;
+
+import com.didactic.clinic.cruddemo.entity.Pacient;
+
+public interface PacientDAO {
+
+	public List<Pacient> getPacients();
+}
